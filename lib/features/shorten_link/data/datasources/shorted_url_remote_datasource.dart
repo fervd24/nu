be1bec3fcc343b405e58d8b605b80fdc1c400f6b
fvd <fvd_24_93@hotmail.com>
2023-07-22 23:46:22 -0600

    Replacing hardcode url, path and headers string for enviroment variables

--- lib/features/shorten_link/data/datasources/shorted_url_remote_datasource.dart
+++ lib/features/shorten_link/data/datasources/shorted_url_remote_datasource.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:nu_test1/core/error/exceptions.dart';
 
 import 'package:nu_test1/features/shorten_link/data/models/short_url_model.dart';
+
 
 abstract class ShortedUrlRemoteDataSource {
   Future<ShortUrlModel?> createShortUrl(String url);
@@ -13,6 +15,7 @@ abstract class ShortedUrlRemoteDataSource {
 class ShortedUrlRemoteDataSourceImpl implements ShortedUrlRemoteDataSource {
   
   final http.Client client;
+  
 
   ShortedUrlRemoteDataSourceImpl({
     required this.client,
@@ -21,16 +24,17 @@ class ShortedUrlRemoteDataSourceImpl implements ShortedUrlRemoteDataSource {
   @override
   Future<ShortUrlModel?> createShortUrl(String url) async {
     
+    await dotenv.load(fileName: ".env");
     final response = await client.post(
-      Uri.https('url-shortener-server.onrender.com', 'api/alias'),
-      headers: {"Content-type": "application/json", "Accept": "application/json"},
+      Uri.https(dotenv.env["URL"]!, dotenv.env["PATH"]!),
+      headers: {
+        "Content-type": dotenv.env["CONTENT_TYPE_HEADER"]!, 
+        "Accept": dotenv.env["ACCEPT_HEADER"]!,
+      },
       body: jsonEncode({
         "url": url
       })
     );
-    // print('data: $url');
-    // print(response.body);
-    // print(response.statusCode);
 
     if(response.statusCode == 200) {
       return ShortUrlModel.fromJson(json.decode(response.body));
